64e98322e1bda13f18e9eddad466a1dc5fa26057
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-23 04:42:01 +0200

    Add arrowBack on NewItemScreen

--- app/src/main/java/com/example/todolist/ui/newitem/NewItemScreen.kt
+++ app/src/main/java/com/example/todolist/ui/newitem/NewItemScreen.kt
@@ -6,8 +6,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -33,7 +37,18 @@ fun NewItemScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Create new task") }
+                title = { Text("Create new task") },
+
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+
+
             )
         }
     ) { padding ->
