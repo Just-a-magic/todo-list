7b7ba25aa027d2587eb122b69d5cd2a6d5814425
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-23 03:30:37 +0200

    Add TodoRepository.kt

---
+++ app/src/main/java/com/example/todolist/data/repository/TodoRepository.kt
@@ -0,0 +1,15 @@
+package com.example.todolist.data.repository
+
+import com.example.todolist.data.local.TodoDao
+import com.example.todolist.data.local.TodoItem
+
+class TodoRepository(private val dao: TodoDao) {
+
+    val items = dao.getAll()
+
+    suspend fun insert(item: TodoItem) = dao.insert(item)
+
+    suspend fun delete(item: TodoItem) = dao.delete(item)
+
+    suspend fun update(item: TodoItem) = dao.update(item)
+}
