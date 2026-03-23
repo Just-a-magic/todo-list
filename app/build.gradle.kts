6f8fd4ad3c5a56a1f49551f1f14155cde1dec7b3
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-23 03:27:48 +0200

    Add dependencies for compose navigation and icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 dependencies {
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
+    implementation(libs.androidx.navigation.compose)
     ksp("androidx.room:room-compiler:2.8.4")
 
     implementation(libs.androidx.core.ktx)
@@ -51,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
