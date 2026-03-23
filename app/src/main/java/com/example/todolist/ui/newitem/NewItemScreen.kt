bde98dc5be8689c2dbe2e6e13d9b487a429a2428
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-23 05:23:03 +0200

    Change text fields on NewItemScreen

--- app/src/main/java/com/example/todolist/ui/newitem/NewItemScreen.kt
+++ app/src/main/java/com/example/todolist/ui/newitem/NewItemScreen.kt
@@ -2,17 +2,19 @@ package com.example.todolist.ui.newitem
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -69,18 +72,22 @@ fun NewItemScreen(
             .padding(padding)
             .padding(16.dp)) {
 
-            TextField(
+            OutlinedTextField(
                 value = title,
                 onValueChange = { title = it },
-                label = { Text("Title") }
+                label = { Text("Title") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
             )
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            TextField(
+            OutlinedTextField(
                 value = description,
                 onValueChange = { description = it },
-                label = { Text("Description") }
+                label = { Text("Description") },
+                modifier = Modifier.fillMaxWidth()
             )
         }
     }
