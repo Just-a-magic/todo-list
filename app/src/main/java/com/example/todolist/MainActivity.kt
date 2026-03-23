00742a1464ecbc59a6d16bd0bcdd5ad429927bf6
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-23 03:34:11 +0200

    Add NavGraph to MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.todolist.ui.theme.ToDoListTheme
+import com.example.todolist.data.local.AppDatabase
+import com.example.todolist.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val db = AppDatabase.getDatabase(this)
+
         enableEdgeToEdge()
         setContent {
-            ToDoListTheme {
-
-            }
+            NavGraph(database = db)
         }
     }
 }
