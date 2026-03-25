9f33cd06b00e5c146d11ed687028ad1c72c532ec
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-25 22:09:01 +0200

    Add bottom sheet for todoItem

--- app/src/main/java/com/example/todolist/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/todolist/ui/home/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.example.todolist.ui.home
 
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,23 +13,31 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.example.todolist.ui.home.HomeViewModel
+import androidx.compose.ui.unit.dp
+import com.example.todolist.data.local.TodoItem
 import com.example.todolist.ui.components.TodoItemView
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel,
-    onAddClick: () -> Unit
+    onAddClick: () -> Unit,
+    onEditClick: (Int) -> Unit
 ) {
     val items by viewModel.items.collectAsState(initial = emptyList())
+
+    var selectedItem by remember { mutableStateOf<TodoItem?>(null) }
 
     Scaffold(
         topBar = {
@@ -33,20 +45,56 @@ fun HomeScreen(
                 title = { Text("Todo List") },
                 actions = {
                     IconButton(onClick = onAddClick) {
-                        Icon(Icons.Default.Add, contentDescription = "Add")
+                        Icon(Icons.Default.Add, null)
                     }
                 }
             )
         }
     ) { padding ->
 
-        LazyColumn(modifier = Modifier.padding(padding)) {
-            items(items) { item ->
-                TodoItemView(
-                    item = item,
-                    onDelete = { viewModel.delete(item) },
-                    onToggle = { viewModel.toggle(item) }
-                )
+        Box(modifier = Modifier.padding(padding)) {
+
+            LazyColumn {
+                items(items) { item ->
+                    TodoItemView(
+                        item = item,
+                        onClick = { onEditClick(item.id) },
+                        onLongClick = { selectedItem = item },
+                        onToggle = { viewModel.toggle(item) }
+                    )
+                }
+            }
+
+            // bottom sheet
+            selectedItem?.let { item ->
+                ModalBottomSheet(
+                    onDismissRequest = { selectedItem = null }
+                ) {
+                    Column {
+
+                        Text(
+                            text = "Edit",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    selectedItem = null
+                                    onEditClick(item.id)
+                                }
+                                .padding(28.dp)
+                        )
+
+                        Text(
+                            text = "Delete",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    viewModel.delete(item)
+                                    selectedItem = null
+                                }
+                                .padding(28.dp)
+                        )
+                    }
+                }
             }
         }
     }
