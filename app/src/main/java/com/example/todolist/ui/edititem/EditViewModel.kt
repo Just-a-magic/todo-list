1d175de75675b9620f3506a096d5bc6bedd285ac
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-25 22:12:40 +0200

    Add EditViewModel

---
+++ app/src/main/java/com/example/todolist/ui/edititem/EditViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.todolist.ui.edititem
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todolist.data.local.TodoItem
+import com.example.todolist.data.repository.TodoRepository
+import kotlinx.coroutines.launch
+
+class EditItemViewModel(
+    private val repository: TodoRepository
+) : ViewModel() {
+
+    var item by mutableStateOf<TodoItem?>(null)
+        private set
+
+    fun load(id: Int) {
+        viewModelScope.launch {
+            item = repository.getById(id)
+        }
+    }
+
+    fun update(title: String, description: String) {
+        val current = item ?: return
+
+        viewModelScope.launch {
+            repository.update(
+                current.copy(
+                    title = title,
+                    description = description
+                )
+            )
+        }
+    }
+}
