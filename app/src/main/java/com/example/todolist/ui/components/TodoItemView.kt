dc9bc0077ee2360777023b8684c166fd754a85ca
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-25 22:06:10 +0200

    Change todoItem

--- app/src/main/java/com/example/todolist/ui/components/TodoItemView.kt
+++ app/src/main/java/com/example/todolist/ui/components/TodoItemView.kt
@@ -1,12 +1,13 @@
 package com.example.todolist.ui.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,27 +17,30 @@ import com.example.todolist.data.local.TodoItem
 @Composable
 fun TodoItemView(
     item: TodoItem,
-    onDelete: () -> Unit,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
     onToggle: () -> Unit
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onToggle() }
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = onLongClick
+            )
             .padding(16.dp)
     ) {
+
         Checkbox(
             checked = item.isDone,
             onCheckedChange = { onToggle() }
         )
 
-        Column(modifier = Modifier.weight(1f)) {
-            Text(item.title)
-            Text(item.description)
-        }
+        Spacer(modifier = Modifier.width(8.dp))
 
-        IconButton(onClick = onDelete) {
-            Text("X")
+        Column {
+            Text(text = item.title)
+            Text(text = item.description)
         }
     }
 }
