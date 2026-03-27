fc602dff915db70c05d347274426e1121ad15b0c
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-27 23:27:58 +0200

    Add Delete all tasks button to SettingsScreen

--- app/src/main/java/com/example/todolist/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/todolist/ui/settings/SettingsScreen.kt
@@ -1,11 +1,12 @@
 package com.example.todolist.ui.settings
 
-
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,6 +16,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,13 +41,17 @@ fun SettingsScreen(
         }
     ) { padding ->
 
-        Box(
+        Column(
             modifier = Modifier
                 .padding(padding)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+                .padding(16.dp)
         ) {
-            Text("Settings")
+            Button(
+                onClick = { showDialog = true },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Delete all tasks")
+            }
         }
     }
 }
