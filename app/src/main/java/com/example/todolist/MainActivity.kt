611df233ab3516fc0a4ccbec8ad31021c2d1e6e8
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-28 02:16:01 +0200

    Add Theme to MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,7 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.example.todolist.data.local.AppDatabase
+import com.example.todolist.data.repository.TodoRepository
 import com.example.todolist.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +23,21 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-            NavGraph(database = db)
+            val repository = remember { TodoRepository(db.todoDao()) }
+
+            val theme by repository.theme.collectAsState()
+
+            val isDark = when (theme) {
+                AppTheme.DARK -> true
+                AppTheme.LIGHT -> false
+                AppTheme.SYSTEM -> isSystemInDarkTheme()
+            }
+
+            MaterialTheme(
+                colorScheme = if (isDark) darkColorScheme() else lightColorScheme()
+            ) {
+            NavGraph(repository = repository)
+            }
         }
     }
 }
