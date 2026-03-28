3357b8a9a4a5547ce0e6658fb13a3a9daf75401f
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-28 02:11:14 +0200

    Add setTheme to SettingsViewModel

--- app/src/main/java/com/example/todolist/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/settings/SettingsViewModel.kt
@@ -2,11 +2,18 @@ package com.example.todolist.ui.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todolist.AppTheme
 import com.example.todolist.data.repository.TodoRepository
 import kotlinx.coroutines.launch
 
 class SettingsViewModel(
     private val repository: TodoRepository) : ViewModel() {
+
+    val theme = repository.theme
+
+    fun setTheme(theme: AppTheme) {
+        repository.setTheme(theme)
+    }
 
     fun deleteAll() {
         viewModelScope.launch {
