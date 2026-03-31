6fc6df57801068c2cde275cb65aef48fcc4eee7b
Viacheslav Starodubov <2000fn3@gmail.com>
2026-03-31 02:02:30 +0200

    Add toDisplayName()

--- app/src/main/java/com/example/todolist/domain/model/AppTheme.kt
+++ app/src/main/java/com/example/todolist/domain/model/AppTheme.kt
@@ -1,7 +1,18 @@
 package com.example.todolist.domain.model
 
+import androidx.compose.runtime.Composable
+
 enum class AppTheme {
     LIGHT,
     DARK,
     SYSTEM
+}
+
+@Composable
+fun AppTheme.toDisplayName(): String {
+    return when (this) {
+        AppTheme.LIGHT -> "Light"
+        AppTheme.DARK -> "Dark"
+        AppTheme.SYSTEM -> "System"
+    }
 }
