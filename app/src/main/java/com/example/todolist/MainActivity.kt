f5b25e5f399f23a55d30fe39fe8b93e1417db61f
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-02 23:33:08 +0200

    Update MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.todolist
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.todolist.data.datastore.SettingsDataStore
 import com.example.todolist.ui.navigation.AppRoot
+import com.example.todolist.ui.utils.applyLanguage
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var settingsDataStore: SettingsDataStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            val currentLang = settingsDataStore.languageFlow.first()
+            applyLanguage(currentLang)
+        }
 
         enableEdgeToEdge()
         setContent {
