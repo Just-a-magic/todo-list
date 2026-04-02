4eba88acaec5a39fbf7c3c3bdc335f46f6b1bd0e
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-02 23:15:01 +0200

    Add stringPreferencesKey

--- app/src/main/java/com/example/todolist/data/datastore/PreferencesKeys.kt
+++ app/src/main/java/com/example/todolist/data/datastore/PreferencesKeys.kt
@@ -4,4 +4,6 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferencesKeys {
     val THEME = stringPreferencesKey("app_theme")
+
+    val LANGUAGE = stringPreferencesKey("app_language")
 }
