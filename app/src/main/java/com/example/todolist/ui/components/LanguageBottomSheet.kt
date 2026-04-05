731fe492248f280c11c803fcc78681586ded68f9
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-05 02:48:13 +0200

    Add title and dismiss button to LanguageBottomSheet

--- app/src/main/java/com/example/todolist/ui/components/LanguageBottomSheet.kt
+++ app/src/main/java/com/example/todolist/ui/components/LanguageBottomSheet.kt
@@ -8,14 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.todolist.R
 import com.example.todolist.domain.model.AppLanguage
 import com.example.todolist.domain.model.toDisplayName
 
@@ -29,13 +33,33 @@ fun LanguageBottomSheet(
     ModalBottomSheet(onDismissRequest = onDismiss) {
 
         Column {
+
+            // sheet title
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = onDismiss,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Back"
+                    )
+                }
+                Text(text = stringResource(R.string.app_language))
+            }
+
             AppLanguage.entries.forEach { lang ->
 
+                // sheet item
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .clickable { onSelect(lang) }
-                        .padding(16.dp),
+                        .padding(24.dp),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(stringResource(lang.toDisplayName()))
@@ -43,7 +67,7 @@ fun LanguageBottomSheet(
                     if (lang == current) {
                         Icon(
                             imageVector = Icons.Default.Check,
-                            contentDescription = null
+                            contentDescription = "Selected"
                         )
                     }
                 }
