d79c9f62ccd086b4031ddb2e99bb78bd939d58f7
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-07 17:03:27 +0200

    Add custom colors

--- app/src/main/java/com/example/todolist/ui/theme/Color.kt
+++ app/src/main/java/com/example/todolist/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.example.todolist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val OrangePrimaryLight = Color(0xFFE16200)
+val OnOrangeLight = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFEAEAEA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFF8F8F8)
+val LightOnSurface = Color(0xFF000000)
+val LightOnSurfaceVariant = Color(0xFF808080)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val OrangePrimaryDark = Color(0xFFFF9765)
+val OnOrangeDark = Color(0xFF000000)
+val DarkBackground = Color(0xFF212121)
+val DarkSurface = Color(0xFF414141)
+val DarkOnSurface = Color(0xFFFFFFFF)
+val DarkSurfaceVariant = Color(0xFF2A2A2A)
+val DarkOnSurfaceVariant = Color(0xFFB9B9B9)
--- app/src/main/java/com/example/todolist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todolist/ui/theme/Theme.kt
@@ -1,26 +1,32 @@
 package com.example.todolist.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = OrangePrimaryLight,
+    onPrimary = OnOrangeLight,
+    background = LightBackground,
+    surface = LightSurface,
+    surfaceVariant = LightSurfaceVariant,
+    onBackground = LightOnSurface,
+    onSurface = LightOnSurface,
+    onSurfaceVariant = LightOnSurfaceVariant
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = OrangePrimaryDark,
+    onPrimary = OnOrangeDark,
+    background = DarkBackground,
+    surface = DarkSurface,
+    onBackground = DarkOnSurface,
+    onSurface = DarkOnSurface,
+    surfaceVariant = DarkSurfaceVariant,
+    onSurfaceVariant = DarkOnSurfaceVariant
+)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -31,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+
 
 @Composable
 fun TodoListTheme(
@@ -46,6 +52,7 @@ fun TodoListTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
