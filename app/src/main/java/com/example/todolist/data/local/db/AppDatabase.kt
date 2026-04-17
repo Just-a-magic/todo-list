754514e8678004c7014ad32eba7bb68d6e1be971
Viacheslav Starodubov <2000fn3@gmail.com>
2026-04-17 23:39:13 +0200

    Remove unused object in AppDatabase

--- app/src/main/java/com/example/todolist/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/todolist/data/local/db/AppDatabase.kt
@@ -1,31 +1,11 @@
 package com.example.todolist.data.local.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.todolist.data.local.db.TodoDao
 import com.example.todolist.data.local.entity.TodoItem
 
 @Database(entities = [TodoItem::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun todoDao(): TodoDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "todo_db"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
