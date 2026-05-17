c4f78424f8361689d413f045a2ff4167418b47b3
Viacheslav Starodubov <2000fn3@gmail.com>
2026-05-17 20:14:44 +0200

    Add selected date state

--- app/src/main/java/com/example/todolist/data/local/entity/TodoItem.kt
+++ app/src/main/java/com/example/todolist/data/local/entity/TodoItem.kt
@@ -2,13 +2,17 @@ package com.example.todolist.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+// эта модель данных определяет структуру таблицы в SQLite
 
 @Entity(tableName = "todo_items")
 data class TodoItem(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    val title: String,
-    val description: String = "",
-    val isDone: Boolean = false,
-    val createdAt: Long = System.currentTimeMillis()
+    val id: Int = 0,                    // id
+    val title: String,                  // название
+    val description: String = "",       // описание
+    val isDone: Boolean = false,        // статус выполнения
+    val createdAt: Long = System.currentTimeMillis(),       // время создания в ms
+    val selectedDate: LocalDate? = null     // выбранная дата
 )
